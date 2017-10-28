42483a869628103413af16ba5f07bd371c9f3dc6
Simon Mc Neil <mcneils@sheridancollege.ca>
2017-10-28 14:56:23 -0400

    Created the basic application following the step-by-step tutorial (as required in part II)

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -3,33 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using PartyInvites.Models;
 
 namespace PartyInvites.Controllers
 {
+    /// <summary>
+    /// Home controllers are were we store all of our related actions together.s
+    /// </summary>
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            //greeting can be anything we want to define, note we don't use this in the final demo
+            ViewBag.Greeting = hour < 12 ? "Goor Morning" : "Good Afternoon"; //we reference the ViewbBag now in MyView
+            return View("MyView");
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
         {
             return View();
         }
 
-        public IActionResult About()
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
         {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
+            if (ModelState.IsValid)
+            {
+                Repository.AddResponse(guestResponse);
+                return View("Thanks", guestResponse);
+            } else
+            {
+                //there is a validation error
+                return View();
+            }
         }
 
-        public IActionResult Contact()
+        public ViewResult ListResponses()
         {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Error()
-        {
-            return View();
+            //calls the static method Responses in the Repository controller
+            return View(Repository.Responses.Where(r => r.WillAttend == true));
         }
     }
 }
---
+++ PartyInvites/Models/GuestResponse.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+/// <summary>
+/// In models we typicall createa are field declarative variables with getter and setters.
+/// Here we also add are validation constraints to each property
+/// </summary>
+namespace PartyInvites.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Please Enter Your Name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Your Email Address")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Please Enter A Valid Email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Your Phone Number")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Please Specify Whether You'll Attend")]
+        public bool? WillAttend { get; set; }
+    }
+}
